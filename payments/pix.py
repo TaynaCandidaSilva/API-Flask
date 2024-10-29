a3551726cb601771e7ecb84e12e63247e5e575f3
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-10-29 15:37:20 -0300

    Finalizando a rota de pagamento pix

--- payments/pix.py
+++ payments/pix.py
@@ -1,26 +1,27 @@
 import uuid
+import qrcode
+
 
 class Pix:
     def __init__(self):
         pass
-    
+
     def create_payment(self):
-        
-        #criar o id do pagamento na instituição financeira
-        bank_payment_id = uuid.uuid4()
-        
-        #codigo copia e cola
-        hash_payment = f'hash_payment_{bank_payment_id}'
-        
-        #QR code
+
+        # criar o id do pagamento na instituição financeira
+
+        bank_payment_id = str(uuid.uuid4())
+
+        # codigo copia e cola
+        hash_payment = f"hash_payment_{bank_payment_id}"
+
+        # QR code
         img = qrcode.make(hash_payment)
-        
-        #Salvar a imagem como PNG
+
+        # Salvar a imagem como PNG
         img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
-        
-        return {"bank_payment_id": bank_payment_id, 
-                    "qr_code_path": "qr_code_payment_{bank_payment_id}"}
-        
-    
-    
-    
+
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": "qr_code_payment_{bank_payment_id}",
+        }
